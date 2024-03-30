e3e56e3005330eba21e701945eac085f3f58d618
EC2 Default User <ec2-user@ip-172-31-36-33.eu-west-1.compute.internal>
2024-03-30 20:34:55 +0000

    tenth commit

--- customer/models.py
+++ customer/models.py
@@ -22,5 +22,12 @@ class OrderModel(models.Model):
     price = models.DecimalField(max_digits=7,decimal_places=2)
     items = models.ManyToManyField('MenuItem', related_name='order', blank=True)
     
+    name = models.CharField(max_length=50,blank=True)
+    email = models.CharField(max_length=50,blank=True)
+    street = models.CharField(max_length=50,blank=True)
+    city = models.CharField(max_length=50,blank=True)
+    State = models.CharField(max_length=15,blank=True)
+    zip_code = models.IntegerField(blank=True, null= True)
+    
     def __str__(self):
         return f'Order: {self.created_on.strftime("%b %d %I: %M %p")}'

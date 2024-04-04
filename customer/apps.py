3f39774b2785023c9e7009d4f4372242ea8ca8a1
EC2 Default User <ec2-user@ip-172-31-36-33.eu-west-1.compute.internal>
2024-04-04 01:22:38 +0000

    admin apps.py commit

--- customer/apps.py
+++ customer/apps.py
@@ -1,6 +1,6 @@
 """ Decides the application configuration """
 from django.apps import AppConfig
 
-""" Defines an AppConfig class for the app """
 class CustomerConfig(AppConfig):
+    """ Defines an AppConfig class for the app """
     name = 'customer'

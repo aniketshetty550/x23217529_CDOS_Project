1ae069ae326590c4a01bb0527c1d655f76374fc1
EC2 Default User <ec2-user@ip-172-31-36-33.eu-west-1.compute.internal>
2024-04-04 01:43:09 +0000

    admin forms.py commit

--- customer/forms.py
+++ customer/forms.py
@@ -1,3 +1,4 @@
+"""  It's used to define Django forms that are specific to the app """
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
@@ -5,12 +6,15 @@ from .models import MenuItem
 
 
 class CreateUserForm(UserCreationForm):
+    """  It allows to access the admin page """
     email = forms.EmailField()
     class Meta:
         model = User
         fields = ['username','email','password1','password2']
+        """ It gives access the admin """
         
 class MenuItemForm(forms.ModelForm):
+    """ It creates Menu Table at the backend """
     class Meta:
         model = MenuItem
         fields = ['name', 'description', 'image', 'price', 'category']
